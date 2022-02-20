d4c2624bf626c537461f8d1c5d124d56aeecefd8
JungGyeongmi <gm950715@gmail.com>
2022-02-20 13:19:31 +0900

    snap update

--- src/codeTest/feb/Snap.java
+++ src/codeTest/feb/Snap.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
+import static java.lang.Math.*;
 
 public class Snap {
 
@@ -12,9 +14,11 @@ public class Snap {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), ",");
-		
-		ArrayList al = new ArrayList();		
-		
+		int check = st.countTokens();
+		List<String> al = new ArrayList<>();		
+		int rand = (int)(Math.random()*100)+1; // 확실하지 않음 맞나?
+		// int rand = 0;
+
 		while(st.hasMoreTokens()) {
 			al.add(st.nextToken().trim());
 		}
@@ -22,12 +26,22 @@ public class Snap {
 		System.out.println("ArrayList : "+al);
 		
 		int ori = al.size();
-		
-		for(int i = 0; i<ori/2; i++) {
-			int idx = (int)(Math.random()*al.size());
-			al.remove(idx);
+		int idx = 0;
+
+		if (check%2 == 0){ // 짝수
+			for(int i = 0; i<ori/2; i++) {
+				idx = (int)(random()*al.size());
+				al.remove(idx);
+			} 
+			
+		} else { // 홀수
+			int controll = rand<=50?ori/2+1:ori/2; // 범위 조절
+			for(int i = 0; i<controll; i++) {
+				idx = (int)(random()*al.size()); // 사이즈가 계속 줄어드니까 이 안에 있어야함
+				al.remove(idx);
+			}
 		}
 		
-		System.out.println("SanpList : "+al);
+		System.out.print("SanpList : "+al);
 	}
 }
