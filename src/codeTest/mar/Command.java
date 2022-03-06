4befbf45caf1fb05b1f20b6704973dc4bf19d379
JungGyeongmi <gm950715@gmail.com>
2022-03-06 19:20:39 +0900

    chore : (Command)주석 삭제 및 if조건문 수정
      if(+inputText[j][i] == +inputText[j+1][i]) { ->   if(+inputText[j][i] != +inputText[j+1][i]) {

--- src/codeTest/mar/Command.java
+++ src/codeTest/mar/Command.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class Command {
     public static void main(String[] args) throws IOException {
 
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N =Integer.parseInt(br.readLine());        
         char[][] inputText = new char[50][50];
@@ -18,17 +19,17 @@ public class Command {
          System.out.println(+inputText[2][7]);
          System.out.println(inputText[2].length);
         */
+        /*
+         System.out.println(i+"다름");
+         System.out.println(result.charAt(i));
+        */
 
+        // 출력
         StringBuilder result = new StringBuilder(String.valueOf(inputText[0]));
-
         for(int i = 0; i < inputText[0].length; i++){
-            for(int j = 0; j < N-1; j++)    {
-                if(+inputText[j][i] == +inputText[j+1][i]) {
-                    // System.out.println("동일");
-                } else{
-                    // System.out.println(result.charAt(i));
+            for(int j = 0; j < N-1; j++) {
+                if(+inputText[j][i] != +inputText[j+1][i]) {
                     result.setCharAt(i, '?');
-                    // System.out.println(i+"다름");
                 }
             }
         }
