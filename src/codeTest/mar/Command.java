470b2cd5392a6be169b30e6d791c1baa2ec6924e
JungGyeongmi <gm950715@gmail.com>
2022-03-06 17:45:54 +0900

    3/6일 코테 할당분 완료
    command 중복되지 않는 글자 '?'로 변경

---
+++ src/codeTest/mar/Command.java
@@ -0,0 +1,37 @@
+package codeTest.mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Command {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =Integer.parseInt(br.readLine());        
+        char[][] inputText = new char[50][50];
+        for(int i = 0; i < N; i++) inputText[i] = br.readLine().toCharArray();
+        
+        /* . => 46 번
+         a => 97
+         z => 122
+         System.out.println(+inputText[2][7]);
+         System.out.println(inputText[2].length);
+        */
+
+        StringBuilder result = new StringBuilder(String.valueOf(inputText[0]));
+
+        for(int i = 0; i < inputText[0].length; i++){
+            for(int j = 0; j < N-1; j++)    {
+                if(+inputText[j][i] == +inputText[j+1][i]) {
+                    // System.out.println("동일");
+                } else{
+                    // System.out.println(result.charAt(i));
+                    result.setCharAt(i, '?');
+                    // System.out.println(i+"다름");
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
