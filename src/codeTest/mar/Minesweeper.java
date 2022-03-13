9fc1ebf3d1a22ef8d3d0af6bdb478f189d27fd49
JungGyeongmi <gm950715@gmail.com>
2022-03-13 19:17:45 +0900

    Minesweeper

--- src/codeTest/mar/Minesweeper.java
+++ src/codeTest/mar/Minesweeper.java
@@ -3,7 +3,6 @@ package codeTest.mar;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Minesweeper {
